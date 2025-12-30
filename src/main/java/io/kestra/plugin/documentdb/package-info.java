a2c83debc34252ad108f8fe6d5a88d4dbe659a51
ben8t <pimpaudben@gmail.com>
2025-12-30 15:36:02 +0100

    chore: update plugin categories

--- src/main/java/io/kestra/plugin/documentdb/package-info.java
+++ src/main/java/io/kestra/plugin/documentdb/package-info.java
@@ -1,4 +1,6 @@
-@PluginSubGroup(title = "DocumentDB", description = "Integrate with DocumentDB, Microsoft's open-source MongoDB-compatible document database built on PostgreSQL.", categories = PluginSubGroup.PluginCategory.DATABASE)
+@PluginSubGroup(title = "DocumentDB", description = "Integrate with DocumentDB, Microsoft's open-source MongoDB-compatible document database built on PostgreSQL.", categories = PluginSubGroup.PluginCategory.DATABASE,
+    categories = { PluginSubGroup.PluginCategory.DATA }
+)
 package io.kestra.plugin.documentdb;
 
 import io.kestra.core.models.annotations.PluginSubGroup;
